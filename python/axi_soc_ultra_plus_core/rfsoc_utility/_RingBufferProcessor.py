f643bab432bbcf5bd64eacc67420c61a1f74ef08
Larry Ruckman <ruckman@slac.stanford.edu>
2022-02-01 19:34:08 -0800

    adding more rfsoc python

---
+++ python/axi_soc_ultra_plus_core/rfsoc_utility/_RingBufferProcessor.py
@@ -0,0 +1,164 @@
+#-----------------------------------------------------------------------------
+# This file is part of the 'axi-soc-ultra-plus-core'. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the 'axi-soc-ultra-plus-core', including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
+# contained in the LICENSE.txt file.
+#-----------------------------------------------------------------------------
+
+import rogue.interfaces.stream as ris
+import pyrogue as pr
+import numpy as np
+import math
+
+# Class for streaming RX
+class RingBufferProcessor(pr.DataReceiver):
+    # Init method must call the parent class init
+    def __init__( self, hidden=True, **kwargs):
+        pr.Device.__init__(self, hidden=hidden, **kwargs)
+        ris.Slave.__init__(self)
+
+        self._rxEnable = False
+        self._maxSize  = 2**14
+        self._adcLsb   = 500.0/float(2**15) # units of mV
+        self._timeBin  = 0.2 # Units of Hz (5GHz sampling)
+        self._freqBin  = (2500.0/float(self._maxSize>>1)) # Units of Hz (5GHz sampling)
+        self._maxAve   = 16
+        self._idx      = 0
+        self._aveSize  = 1
+
+        # Calculate the time/frequency x-axis arrays
+        timeSteps = np.linspace(0, self._timeBin*(self._maxSize-1), num=self._maxSize)
+        freqSteps = np.linspace(0, self._freqBin*((self._maxSize>>1)-1), num=(self._maxSize>>1))
+
+        self.add(pr.LocalVariable(
+            name        = 'RxEnable',
+            description = 'Frame Rx Enable',
+            value       = True,
+        ))
+
+        self.add(pr.LocalVariable(
+            name         = 'FrameCount',
+            description  = 'Frame Rx Counter',
+            value        = 0,
+            pollInterval = 1,
+        ))
+
+        self.add(pr.LocalVariable(
+            name         = 'ErrorCount',
+            description  = 'Frame Error Counter',
+            value        = 0,
+            pollInterval = 1,
+        ))
+
+        self.add(pr.LocalVariable(
+            name         = 'ByteCount',
+            description  = 'Byte Rx Counter',
+            value        = 0,
+            pollInterval = 1,
+        ))
+
+        self.add(pr.LocalVariable(
+            name        = 'Time',
+            description = 'Time steps (ns)',
+            typeStr     = 'Float[np]',
+            value       = timeSteps,
+            hidden      = True,
+        ))
+
+        self.add(pr.LocalVariable(
+            name        = 'Data',
+            description = 'Data Frame Container',
+            typeStr     = 'Int16[np]',
+            value       = np.zeros(shape=self._maxSize, dtype=np.int16, order='C'),
+            hidden      = True,
+        ))
+
+        self.add(pr.LocalVariable(
+            name        = 'Freq',
+            description = 'Freq steps (ns)',
+            typeStr     = 'Float[np]',
+            value       = freqSteps,
+            hidden      = True,
+        ))
+
+        self.add(pr.LocalVariable(
+            name        = 'Magnitude',
+            description = 'Magnitude Frame Container',
+            typeStr     = 'Float[np]',
+            value       = np.zeros(shape=self._maxSize, dtype=np.float, order='C'),
+            hidden      = True,
+        ))
+
+        self.add(pr.LocalVariable(
+            name        = 'FftAveraging',
+            description = 'Number of FFTs to average together',
+            localSet    = self._fftAveraging,
+            mode        = 'RW',
+            typeStr     = 'UInt12',
+            value       = self._maxAve,
+            minimum     = 1,
+            maximum     = self._maxAve,
+        ))
+
+        self._mag = np.zeros(shape=[self._maxAve,(self._maxSize>>1)], dtype=np.float, order='C')
+
+    def _fftAveraging(self,value,changed):
+        if changed:
+            self.FftAveraging.set(int(value))
+            self.rstFftAveraging()
+
+    def rstFftAveraging(self):
+        self._idx     = 0
+        self._aveSize = 1
+
+    def running_mean(self,x):
+        # Calculate running average
+        retVar = np.sum(x[:self._aveSize], axis=0, dtype=np.float)/float(self._aveSize)
+
+        # Increment counters
+        if (self._idx == self.FftAveraging.get()-1):
+            self._idx = 0
+        else:
+            self._idx += 1
+        if (self._aveSize != self.FftAveraging.get()):
+            self._aveSize += 1
+
+        # Return the results
+        return retVar
+
+    # Method which is called when a frame is received
+    def process(self,frame):
+        if self.RxEnable.get():
+            # Get payload size (int16)
+            size = (frame.getPayload() >>1)
+
+            # Check if too big
+            size = self._maxSize if (size >= self._maxSize) else size
+
+            # To access the data we need to create a byte array to hold the data
+            fullData = bytearray(size<<1)
+
+            # Next we read the frame data into the byte array, from offset 0
+            frame.read(fullData,0)
+
+            # Get data from frame
+            data = np.frombuffer(fullData, dtype='int16', count=size)
+            self.Data.set(data,write=True)
+
+            # Calculate the FFT
+            freq = np.fft.fft(data)/float(self._maxSize)
+            freq = freq[range(self._maxSize>>1)]
+
+            # Prevent warning message when for divide by zero encountered in log10
+            # Checking for inf later to fix this in the display
+            np.seterr(divide = 'ignore')
+
+            # Calculate the average magnitude
+            mag = 20.0*np.log10(np.abs(freq)/32767.0) # Units of dBFS
+            if sum(np.isinf(mag)) == 0:
+                self._mag[self._idx] = mag
+                magnitude = self.running_mean(self._mag)
+                self.Magnitude.set(magnitude,write=True)
---
+++ python/axi_soc_ultra_plus_core/rfsoc_utility/gui/GuiTop.py
@@ -0,0 +1,81 @@
+#-----------------------------------------------------------------------------
+# This file is part of the 'SPACE SMURF RFSOC'. It is subject to
+# the license terms in the LICENSE.txt file found in the top-level directory
+# of this distribution and at:
+#    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+# No part of the 'SPACE SMURF RFSOC', including this file, may be
+# copied, modified, propagated, or distributed except according to the terms
+# contained in the LICENSE.txt file.
+#-----------------------------------------------------------------------------
+
+import os
+from pydm import Display
+from qtpy.QtWidgets import (QVBoxLayout, QTabWidget)
+
+from pyrogue.pydm.widgets import DebugTree
+from pyrogue.pydm.widgets import SystemWindow
+
+Channel = 'rogue://0/root'
+
+import axi_soc_ultra_plus_core.rfsoc_utility.gui as gui
+
+class GuiTop(Display):
+    def __init__(self, parent=None, args=[], macros=None):
+        super(GuiTop, self).__init__(parent=parent, args=args, macros=None)
+
+        self.setStyleSheet("*[dirty='true']\
+                           {background-color: orange;}")
+
+        self.sizeX  = None
+        self.sizeY  = None
+        self.title  = None
+
+        for a in args:
+            if 'sizeX=' in a:
+                self.sizeX = int(a.split('=')[1])
+            if 'sizeY=' in a:
+                self.sizeY = int(a.split('=')[1])
+            if 'title=' in a:
+                self.title = a.split('=')[1]
+
+        if self.title is None:
+            self.title = "Rogue Server: {}".format(os.getenv('ROGUE_SERVERS'))
+
+        if self.sizeX is None:
+            self.sizeX = 800
+        if self.sizeY is None:
+            self.sizeY = 1000
+
+        self.setWindowTitle(self.title)
+
+        vb = QVBoxLayout()
+        self.setLayout(vb)
+
+        self.tab = QTabWidget()
+        vb.addWidget(self.tab)
+
+        # Live Display (Tab Index=0)
+        sys = SystemWindow(parent=None, init_channel=Channel)
+        self.tab.addTab(sys,'System')
+
+        # Live Display (Tab Index=1)
+        var = DebugTree(parent=None, init_channel=Channel)
+        self.tab.addTab(var,'Debug Tree')
+
+        # ADC Live Display (Tab Index=2)
+        adcDisplay = gui.LiveDisplay(parent=None, init_channel=Channel, dispType='Adc')
+        self.tab.addTab(adcDisplay,'ADC Waveforms')
+
+        # DAC Live Display (Tab Index=3)
+        dacDisplay = gui.LiveDisplay(parent=None, init_channel=Channel, dispType='Dac')
+        self.tab.addTab(dacDisplay,'DAC Waveforms')
+
+        # Set the default Tab view
+        self.tab.setCurrentIndex(1)
+
+        # Resize the window
+        self.resize(self.sizeX, self.sizeY)
+
+    def ui_filepath(self):
+        # No UI file is being used
+        return None
