d46475b37ea8d9fd6f31e5cf712679ce7ff1c29b
Larry Ruckman <ruckman@slac.stanford.edu>
2023-04-04 09:13:05 -0700

    fixing code headers

--- python/axi_soc_ultra_plus_core/rfsoc_utility/pydm/__init__.py
+++ python/axi_soc_ultra_plus_core/rfsoc_utility/pydm/__init__.py
@@ -1,11 +1,11 @@
 #-----------------------------------------------------------------------------
 # Title      : PyRogue PyDM Package, Function to start default Rogue PyDM GUI
 #-----------------------------------------------------------------------------
-# This file is part of the rogue software platform. It is subject to
+# This file is part of the 'axi-soc-ultra-plus-core'. It is subject to
 # the license terms in the LICENSE.txt file found in the top-level directory
 # of this distribution and at:
 #    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
-# No part of the rogue software platform, including this file, may be
+# No part of the 'axi-soc-ultra-plus-core', including this file, may be
 # copied, modified, propagated, or distributed except according to the terms
 # contained in the LICENSE.txt file.
 #-----------------------------------------------------------------------------
