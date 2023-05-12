c58dfd0993a6738fac63ba0ef1b24757dd723e49
Larry Ruckman <ruckman@slac.stanford.edu>
2023-05-12 10:49:40 -0700

    adding def pingCheck(ip)

--- python/axi_soc_ultra_plus_core/__init__.py
+++ python/axi_soc_ultra_plus_core/__init__.py
@@ -1,3 +1,14 @@
 from axi_soc_ultra_plus_core._AxiVersion     import *
 from axi_soc_ultra_plus_core._SysMonLvAuxDet import *
 from axi_soc_ultra_plus_core._AxiSocCore     import *
+
+import click
+import subprocess
+
+def pingCheck(ip):
+    try:
+        subprocess.check_output(["ping", "-c", "1", ip])
+    except subprocess.CalledProcessError:
+        errMsg = f'Failied to ping {ip}'
+        click.secho(errMsg, bg='red')
+        raise ValueError(errMsg)
