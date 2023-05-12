b9102f9e47f86d5eda64aa8e2c82c1f87a2981e8
Larry Ruckman <ruckman@slac.stanford.edu>
2023-05-12 11:46:29 -0700

    adding TCP port checking

--- python/axi_soc_ultra_plus_core/__init__.py
+++ python/axi_soc_ultra_plus_core/__init__.py
@@ -5,10 +5,22 @@ from axi_soc_ultra_plus_core._AxiSocCore     import *
 import click
 import subprocess
 
-def pingCheck(ip):
+def connectionTest(ip):
     try:
         subprocess.check_output(["ping", "-c", "1", ip])
+        portStatus = subprocess.getoutput(f'netstat -t | grep {ip}:9000 | grep ESTABLISHED')
+        if portStatus != '':
+            errMsg = f"""
+                {portStatus}
+                Failied to open {ip} at port=9000
+                Likely another thread has the software running
+                """
+            click.secho(errMsg, bg='red')
+            raise ValueError(errMsg)
     except subprocess.CalledProcessError:
-        errMsg = f'Failied to ping {ip}'
+        errMsg = f"""
+            Failied to ping {ip}
+            Double check the IP address and isc-dhcp-server.service is running
+            """
         click.secho(errMsg, bg='red')
         raise ValueError(errMsg)
