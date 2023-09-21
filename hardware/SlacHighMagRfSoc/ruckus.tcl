e6c5c13b23b8df4370c5497e95fe5c94420e7cee
Larry Ruckman <ruckman@slac.stanford.edu>
2023-09-21 12:11:12 -0700

    switching to high-mag-rfsoc platform

--- hardware/SlacHighMagRfSoc/ruckus.tcl
+++ hardware/SlacHighMagRfSoc/ruckus.tcl
@@ -2,8 +2,8 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Check for valid FPGA
-if { $::env(PRJ_PART) != "xczu48dr-ffvg1517-1-e" && $::env(PRJ_PART) != "xqzu48dr-fsrg1517-1M-m" } {
-   puts "\n\nERROR: PRJ_PART must be either xczu48dr-ffvg1517-1-e or xqzu48dr-fsrg1517-1M-m in the Makefile\n\n"; exit -1
+if { $::env(PRJ_PART) != "xczu48dr-ffvg1517-1-e" } {
+   puts "\n\nERROR: PRJ_PART must be either xczu48dr-ffvg1517-1-e in the Makefile\n\n"; exit -1
 }
 
 # Load shared source code
