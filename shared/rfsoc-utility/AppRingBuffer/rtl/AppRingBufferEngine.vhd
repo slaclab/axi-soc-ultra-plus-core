aef2e50869283de442ed8bfb4cfe7f97aaf6e615
Larry Ruckman <ruckman@slac.stanford.edu>
2023-10-19 19:21:22 -0700

    adding MEMORY_TYPE_G & COMMON_CLK_G to AppRingBuffer.vhd

--- shared/rfsoc-utility/AppRingBuffer/rtl/AppRingBufferEngine.vhd
+++ shared/rfsoc-utility/AppRingBuffer/rtl/AppRingBufferEngine.vhd
@@ -29,6 +29,8 @@ entity AppRingBufferEngine is
       NUM_CH_G           : positive               := 8;
       SAMPLE_PER_CYCLE_G : positive               := 16;
       RAM_ADDR_WIDTH_G   : positive               := 10;
+      MEMORY_TYPE_G      : string                 := "block";
+      COMMON_CLK_G       : boolean                := false;  -- true if dataClk=axilClk
       TDEST_ROUTES_G     : Slv8Array(15 downto 0) := (others => x"00");
       AXIL_BASE_ADDR_G   : slv(31 downto 0));
    port (
@@ -125,6 +127,8 @@ begin
          generic map (
             TPD_G               => TPD_G,
             SYNTH_MODE_G        => "xpm",
+            MEMORY_TYPE_G       => MEMORY_TYPE_G,
+            COMMON_CLK_G        => COMMON_CLK_G,
             DATA_BYTES_G        => ((16*SAMPLE_PER_CYCLE_G)/8),
             RAM_ADDR_WIDTH_G    => RAM_ADDR_WIDTH_G,
             -- AXI Stream Configurations
