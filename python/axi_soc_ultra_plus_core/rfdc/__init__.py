98395c982945dae6795575053aed384f0a136325
Larry Ruckman <ruckman@slac.stanford.edu>
2024-06-05 00:57:58 +0000

    removing files

--- python/axi_soc_ultra_plus_core/rfdc/__init__.py
+++
@@ -1,6 +0,0 @@
-from axi_soc_ultra_plus_core.rfdc.xrfdc_hw import *
-from axi_soc_ultra_plus_core.rfdc.xrfdc import *
-
-from axi_soc_ultra_plus_core.rfdc._RfTile              import *
-from axi_soc_ultra_plus_core.rfdc._RfBlock             import *
-from axi_soc_ultra_plus_core.rfdc._RfDataConverter     import *
