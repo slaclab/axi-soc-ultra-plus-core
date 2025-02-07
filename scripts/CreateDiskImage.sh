6f4037334d52d1b8029ef9af0556a37cb17de371
Larry Ruckman <ruckman@slac.stanford.edu>
2025-02-07 08:09:27 -0800

    adding license header

--- scripts/CreateDiskImage.sh
+++ scripts/CreateDiskImage.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+##############################################################################
+## This file is part of 'axi-soc-ultra-plus-core'.
+## It is subject to the license terms in the LICENSE.txt file found in the
+## top-level directory of this distribution and at:
+##    https://confluence.slac.stanford.edu/display/ppareg/LICENSE.html.
+## No part of 'axi-soc-ultra-plus-core', including this file,
+## may be copied, modified, propagated, or distributed except according to
+## the terms contained in the LICENSE.txt file.
+##############################################################################
+
 # First argument is output image file
 # Second argument points to file produced by CreatePetalinuxProject.sh
 # Use as ./CreateDiskImage.sh image.img <FileFromYourBuild>.petalinux.tar
