fd71c1c66abbab0bb2ed40f14a1c18d98fa27b52
Larry Ruckman <ruckman@slac.stanford.edu>
2025-08-15 08:49:50 -0700

    Update BuildYoctoProject.sh

--- BuildYoctoProject.sh
+++ BuildYoctoProject.sh
@@ -9,6 +9,9 @@
 ## the terms contained in the LICENSE.txt file.
 ##############################################################################
 
+## Clear terminal output
+#echo -ne "\033c"
+
 while getopts p:n:h:x:l:d:t:r:s:f: flag
 do
     case "${flag}" in
