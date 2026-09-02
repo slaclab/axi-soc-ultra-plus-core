8cb04c7bbea6f09766dc9b01283638b5118229d1
Larry Ruckman <ruckman@slac.stanford.edu>
2026-09-02 11:58:29 -0700

    chore(SlacRfmcCarrier): rebuild the block design and DDR4 IP in Vivado 2026.1
    Regenerate AxiSocUltraPlusCpuCore and the MigCore DDR4 IP under Vivado
    2026.1, the version already used by XilinxKriaKv260, and move the block
    design from bd/2025.1 to bd/2026.1.
    
    Tool version, IP revision and generated output path fields only, no
    functional change to either memory interface.

--- hardware/SlacRfmcCarrier/ruckus.tcl
+++ hardware/SlacRfmcCarrier/ruckus.tcl
@@ -1,8 +1,8 @@
 # Load RUCKUS environment and library
 source $::env(RUCKUS_PROC_TCL)
 
-# Check for version 2025.1 of Vivado (or later)
-if { [VersionCheck 2025.1] < 0 } {exit -1}
+# Check for version 2026.1 of Vivado (or later)
+if { [VersionCheck 2026.1] < 0 } {exit -1}
 
 # Check for valid FPGA
 if { $::env(PRJ_PART) != "XCZU48DR-FFVG1517-2-E" } {
@@ -15,8 +15,8 @@ loadConstraints -dir "$::DIR_PATH/xdc"
 loadSource -lib axi_soc_ultra_plus_core -dir "$::DIR_PATH/rtl"
 
 # Load the block design
-if  { $::env(VIVADO_VERSION) >= 2025.1 } {
-   set bdVer "2025.1"
+if  { $::env(VIVADO_VERSION) >= 2026.1 } {
+   set bdVer "2026.1"
 }
 loadBlockDesign -path "$::DIR_PATH/bd/${bdVer}/AxiSocUltraPlusCpuCore.bd"
 # loadBlockDesign -path "$::DIR_PATH/bd/${bdVer}/AxiSocUltraPlusCpuCore.tcl"
